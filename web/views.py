0dd1487ae30c0aa0a04f9f6a1c0c7154899523c0
CYBER NETICS <cybernetics.me@outlook.com>
2022-01-18 18:48:44 +0530

    Error fixed for product count

--- web/views.py
+++ web/views.py
@@ -16,13 +16,15 @@ def index(request):
 def products(request, collection_slug=None):
     collection = None
     products = None
+    products_count = 0
     if collection_slug != None:
         collection = get_object_or_404(Collection, slug=collection_slug)
         products = Product.objects.all().filter(collection = collection, is_available = True)
         products_count = products.count()
     else:
         products = Product.objects.all().filter(is_available = True)
-        products_count = products.count()
+        if(products.count() > 0):
+            products_count = products.count()
     
     context = {
         'collection': collection,

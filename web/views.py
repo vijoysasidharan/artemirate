ec998b631f73eb863aeda553ffef546191902b5a
CYBER NETICS <cybernetics.me@outlook.com>
2022-01-18 20:46:56 +0530

    Small Update

--- web/views.py
+++ web/views.py
@@ -16,15 +16,13 @@ def index(request):
 def products(request, collection_slug=None):
     collection = None
     products = None
-    products_count = 0
     if collection_slug != None:
         collection = get_object_or_404(Collection, slug=collection_slug)
         products = Product.objects.all().filter(collection = collection, is_available = True)
-        #products_count = products.count()
+        products_count = products.count()
     else:
         products = Product.objects.all().filter(is_available = True)
-        #if(products.count() > 0):
-            #products_count = products.count()
+        products_count = products.count()
     
     context = {
         'collection': collection,

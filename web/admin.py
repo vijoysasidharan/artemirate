b8f24b9206e3267c240462e0880d71bb2660509b
CYBER NETICS <cybernetics.me@outlook.com>
2022-02-21 16:40:50 +0530

    small updates

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from web.models import *
 
 # Register your models here.
+
+class MaterialAdmin(admin.ModelAdmin):
+    list_display = ('id', 'material_name')
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'type_name')
+
+class ShapeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'shape_name')
+
+class UsageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usage_name')
+
+
+admin.site.register(ProductMaterial, MaterialAdmin)
+admin.site.register(ProductType, TypeAdmin)
+admin.site.register(ProductShape, ShapeAdmin)
+admin.site.register(ProductUsage, UsageAdmin)
